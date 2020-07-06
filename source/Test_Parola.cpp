5dc4efc302723ea031cc4d314a725b034996aa64
JojoS62 <sn2010@onlinehome.de>
2020-07-06 21:07:20 +0200

    cleanup

--- source/Test_Parola.cpp
+++ source/Test_Parola.cpp
@@ -6,13 +6,9 @@ MD_Parola P(MD_MAX72XX::FC16_HW, PA_7, PA_5, PA_1, 4);
 
 void setup(void)
 {
-  P.begin();
-//  P.displayText("Schalke", PA_CENTER, 0, 0, PA_PRINT, PA_NO_EFFECT);
-  P.displayScroll("Das ist die Einschaltmeldung.", PA_RIGHT, PA_SCROLL_LEFT, 50);
+    P.begin();
+    P.displayScroll("Das ist die Einschaltmeldung.", PA_RIGHT, PA_SCROLL_LEFT, 50);
 }
-
-int intensity = 8;
-int delta = -1;
 
 void loop(void)
 {
