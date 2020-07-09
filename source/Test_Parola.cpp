5dc8469128048d104cd653fcf1792877738be386
JojoS62 <sn2010@onlinehome.de>
2020-07-10 00:35:53 +0200

    test printf method

--- source/Test_Parola.cpp
+++ source/Test_Parola.cpp
@@ -4,9 +4,13 @@
 
 MD_Parola P(MD_MAX72XX::FC16_HW, PA_7, PA_5, PA_1, 4);
 
+char frameBuffer[64];
+static int counter = 0;
+
 void setup(void)
 {
     P.begin();
+    P.setFrameBuffer(frameBuffer, sizeof(frameBuffer));
     P.displayScroll("Das ist die Einschaltmeldung.", PA_RIGHT, PA_SCROLL_LEFT, 50);
 }
 
@@ -14,7 +18,7 @@ void loop(void)
 {
     if (P.displayAnimate()) {
         // animation done, restart
-        P.displayScroll("Schalke ist der geilste Club der Welt :)", PA_RIGHT, PA_SCROLL_LEFT, 50);
+        P.printf("%i: Schalke ist der geilste Club der Welt :)\n", counter++);
     }
 
     ThisThread::sleep_for(10ms);
