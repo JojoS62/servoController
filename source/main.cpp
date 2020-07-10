2e2257e176bd322d05db3e17727747ac298e77a4
JojoS62 <sn2010@onlinehome.de>
2020-07-10 16:13:53 +0200

    change to test-lfs

--- source/main.cpp
+++ source/main.cpp
@@ -1,101 +1,50 @@
 #include "mbed.h"
-#include "console.h"
-#include "Test_MAX72xx.h"
 
-DigitalIn  btn0(USER_BUTTON, PullUp);
-int btnOld;
-DigitalOut led1(LED1);
-PwmOut servoTurn(PB_10);
-PwmOut servoTilt(PB_1);
-int posTurn = 1500;
-int posTilt = 1500;
-int stepSize = 50;
+#include "SPIFBlockDevice.h"
+#include "littlefsv2/LittleFileSystem2.h"
+//#include "storage/filesystem/littlefs/LittleFileSystem.h"
 
-EventQueue *queue = mbed_event_queue(); // shared event queue;
-uint8_t buffer[32+1];
-uint8_t *bufptr = buffer;
+using namespace std::chrono;
 
-BufferedSerial  serial(STDIO_UART_TX, STDIO_UART_RX, 115200);
+SPIFBlockDevice spif(PA_7, PA_6, PA_5, PA_4);
+LittleFileSystem2 fs("fs");
 
-Console console(&serial);
-
-void messageComplete()
-{
-    led1 = !led1;
-    printf("message: %s\n", buffer);
-    bufptr = buffer;
-
-    switch (buffer[0]) {
-        case 's': if (posTurn > 1000) posTurn -= stepSize; break;
-        case 'd': if (posTurn < 2000) posTurn += stepSize; break;
-        case 'a': if (posTilt > 1000) posTilt -= stepSize; break;
-        case 'y': if (posTilt < 2000) posTilt += stepSize; break;
-    }
-}
-
-void on_rx_interrupt()
-{
-    int nbytes = serial.read(bufptr, 1);
-    bufptr += nbytes;
-    if (bufptr[-1] == '\n' || bufptr[-1] == '\r') {
-        *bufptr = '\0';
-        queue->call(messageComplete);
-    }
-}
-
-int main() 
+int main()
 {
     printf("Hello from "  MBED_STRINGIFY(TARGET_NAME) "\n");
     printf("Mbed OS version: %d.%d.%d\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
- 
-    // Register a callback to process a Rx (receive) interrupt.
-    //serial.attach(&on_rx_interrupt, SerialBase::RxIrq);
 
-    servoTurn.period_ms(20);
-    servoTurn.pulsewidth_us(posTurn);
+    spif.init();
+    printf("spif size: %llu\n",         spif.size());
+    printf("spif read size: %llu\n",    spif.get_read_size());
+    printf("spif program size: %llu\n", spif.get_program_size());
+    printf("spif erase size: %llu\n",   spif.get_erase_size());
 
-    servoTilt.period_ms(20);
-    servoTilt.pulsewidth_us(posTilt);
+    int err = 0;
 
-    setup();
-    while(true) {
-        loop();
+    err = fs.format(&spif);
+    debug_if(err != 0, "format error: %d\n", err);
+
+    err = fs.mount(&spif);
+    debug_if(err != 0, "mount error: %d\n", err);
+
+    printf("write file  fileNo  time [ms]\n");
+    char fileName[64];
+    for (int i=0; i < 1000; i++) {
+        auto startTime = Kernel::Clock::now(); 
+
+        snprintf(fileName, sizeof(fileName), "/fs/file_%d.txt", i);
+
+        FILE *f = fopen(fileName, "w+");
+        err = fprintf(f, "123456789012\n");
+        debug_if(err <= 0, "write error: %d\n", err);
+
+        err = fclose(f);
+        debug_if(err != 0, "close error: %d\n", err);
+
+        milliseconds elapsed_time = Kernel::Clock::now() - startTime;
+        printf("%8d %8d ms\n", i, int(elapsed_time.count()));
     }
 
-    btnOld = btn0;
-    // dispatch events
-    while(1) {
-
-        int btn = btn0;
-
-        if (btn != btnOld && btn==1) {
-            led1 = !led1;
-        }
-        btnOld = btn;
-
-#if 0
-        char ch = getc(stdin);
-        if (ch >= '0' && ch <= '9') {
-            //posTurn = (ch - '0') * 100 + 1000;
-            stepSize = (ch - '0' + 1) * 10;
-        }
-
-        switch (ch) {
-            case 's': if (posTurn > 1000) posTurn -= stepSize; break;
-            case 'd': if (posTurn < 2000) posTurn += stepSize; break;
-            case 'a': if (posTilt > 1000) posTilt -= stepSize; break;
-            case 'y': if (posTilt < 2000) posTilt += stepSize; break;
-        }
-#endif
-
-        //queue->dispatch(20);
-        console.process();      // blocking for now
-
-        servoTurn.pulsewidth_us(posTurn);
-        servoTilt.pulsewidth_us(posTilt);
-
-        //ThisThread::sleep_for(20ms);
-        //queue->dispatch(50);
-    }
+    return 0;
 }
-
